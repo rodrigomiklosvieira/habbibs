24f3dad444b27a03ea228eada0d4f1d216ce0cc3
Enoque Felipe <>
2018-10-29 18:46:07 -0200

    First commit

---
+++ src/br/com/habbibs/Main.java
@@ -0,0 +1,19 @@
+package br.com.habbibs;
+
+/**
+ *
+ * @author enoqueleal
+ */
+public class Main {
+    
+    /**
+     * Main method for running application
+     * @param args 
+     */
+    public static void main(String[] args){
+        System.out.println("Sistema criado com sucesso.");
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+    }
+    
+}
