5b03962b56333fa39e7eabf62c9e627fde0aab54
Rodrigo Miklos Vieira <rodrigo.vieira@etec.sp.gov.br>
2018-11-05 19:52:40 -0200

    Criei uma nova classe Produto com dois atributos pelo método get set.

---
+++ src/br/com/habbibs/Produto.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.habbibs;
+
+/**
+ *
+ * @author LAB
+ */
+public class Produto {
+    
+    public String nome;
+    public double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+    
+    
+}
